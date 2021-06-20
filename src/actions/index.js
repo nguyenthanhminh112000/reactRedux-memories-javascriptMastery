674c47325ce7eb52ba831bd828526ac67902f28e
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2021-06-20 16:19:03 +0700

    add constants and format code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,12 @@
 // import dependencies
 import * as api from './../api/index.js';
+import {
+  FETCH_ALL,
+  CREATE,
+  UPDATE,
+  DELETE,
+  LIKE,
+} from './../constants/actionTypes.js';
 
 // action creators
 //just work for sync-function
@@ -9,7 +16,7 @@ export const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts();
     console.log('getPosts action-creator');
-    dispatch({ type: 'FETCH_ALL', payload: data });
+    dispatch({ type: FETCH_ALL, payload: data });
   } catch (error) {
     console.error(error.message);
   }
@@ -19,7 +26,7 @@ export const createPost = (post) => async (dispatch) => {
   try {
     const { data } = await api.createPost(post);
     console.log('createPost action-creator');
-    dispatch({ type: 'CREATE', payload: data });
+    dispatch({ type: CREATE, payload: data });
   } catch (error) {
     console.log(error);
   }
@@ -28,7 +35,7 @@ export const updatePost = (id, post) => async (dispatch) => {
   try {
     const { data } = await api.updatePost(id, post);
     console.log('updatePost action-creator');
-    dispatch({ type: 'UPDATE', payload: data });
+    dispatch({ type: UPDATE, payload: data });
   } catch (error) {
     console.log(error);
   }
@@ -38,7 +45,7 @@ export const deletePost = (id) => async (dispatch) => {
   try {
     await api.deletePost(id);
     console.log('deletePost action-creator');
-    dispatch({ type: 'DELETE', payload: id });
+    dispatch({ type: DELETE, payload: id });
   } catch (error) {
     console.log(error);
   }
@@ -47,6 +54,8 @@ export const deletePost = (id) => async (dispatch) => {
 export const likePost = (id) => async (dispatch) => {
   try {
     const { data } = await api.likePost(id);
-    dispatch({ type: 'LIKE', payload: data });
-  } catch (error) {}
+    dispatch({ type: LIKE, payload: data });
+  } catch (error) {
+    console.log(error);
+  }
 };
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,15 +1,22 @@
+import {
+  FETCH_ALL,
+  CREATE,
+  UPDATE,
+  DELETE,
+  LIKE,
+} from './../constants/actionTypes.js';
 const postsReducer = (state = [], action) => {
   switch (action.type) {
-    case 'FETCH_ALL':
+    case FETCH_ALL:
       return action.payload;
-    case 'CREATE':
+    case CREATE:
       return [...state, action.payload];
-    case 'UPDATE':
-    case 'LIKE':
+    case UPDATE:
+    case LIKE:
       return state.map((post) =>
         post._id === action.payload._id ? action.payload : post
       );
-    case 'DELETE':
+    case DELETE:
       return state.filter((post) => post._id !== action.payload);
     default:
       return state;
